bc52113704af01ecd1965f4af3ddc0967af6bd2f
idaxstr <ida.x.streubel@delwp.vic.gov.au>
2024-01-25 16:29:29 +1100

    first commit

---
+++ Lesson 2/assignment_2_ida.py
@@ -0,0 +1,5 @@
+original_text = "invalid syntax! "
+shouting_text = original_text.upper()
+annoying_text = shouting_text * 10
+print(f'My annoying text is {annoying_text}')
+print(f'It has {len(annoying_text)} characters')
