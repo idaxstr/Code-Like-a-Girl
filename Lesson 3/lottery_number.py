13ec26add9ce1cd51a690a42700c6cabda5aa887
idaxstr <ida.x.streubel@delwp.vic.gov.au>
2024-01-29 15:48:27 +1100

    first commit

---
+++ Lesson 3/lottery_number.py
@@ -0,0 +1,9 @@
+my_number = 8
+lottery_number = 7
+is_match = my_number == lottery_number
+print(is_match)
+print(type(is_match))
+
+dividend = int('10')
+divisor = int('5')
+print(dividend/divisor)
---
+++ Lesson 3/pizza.py
@@ -0,0 +1,5 @@
+coupon_num = 5
+pizza_slice = 5 * 12
+print(pizza_slice)
+people = input('Enter the number of people')
+print(people)
