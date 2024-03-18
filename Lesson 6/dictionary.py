5a06121e345b5a828e9eedb9fcd0791ad8773aaa
idaxstr <ida.x.streubel@delwp.vic.gov.au>
2024-03-18 15:48:16 +1100

    first commit

---
+++ Lesson 6/dictionary.py
@@ -0,0 +1,13 @@
+ida = {'age':'31', 'occupation': 'government', 'hobby': 'skiing'}
+print(ida)
+
+ida['pet'] = 'Raspi'
+print(ida)
+
+for key, value in ida.items():
+    print('My ' +key, 'is ' +value)
+    
+    
+    
+          
+
